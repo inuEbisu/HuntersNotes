78be31ed083423bd351a5bafc49cbd4cef64fb5f
犬戎 <inuebisu@qq.com>
2025-02-21 00:23:21 +0800

    fix: second try

--- cache.ts
+++ cache.ts
@@ -1,4 +1,7 @@
-import { launch } from "vite-plugin-vue-xecades-note";
+import { tsImport } from "tsx/esm/api";
+
+// Thx to motherfxxking Node.js TypeScript support
+const launch: typeof import("vite-plugin-vue-xecades-note/src/index").launch = (await tsImport("vite-plugin-vue-xecades-note/src/index", import.meta.url)).launch;
 
 launch({
     componentDir: "src/components/md",
